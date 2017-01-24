3d39028de9f2b5ffac158559ee1bd9bd4a29c167
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-01-24 13:09:15 +0900

    起動時コメント追加

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ new CronJob('')
 
 var settings = require('./setting.json');
 
+post("起動しますた");
 var startLesson = new CronJob("0 30 10-16 * * 1-5",function(){post("授業開始だよ!")},null,true,"Asia/Tokyo")
 var endLesson = new CronJob("0 20 11-16 * * 1-5",function(){post("休み時間だよ!")},null,true,"Asia/Tokyo")
 
