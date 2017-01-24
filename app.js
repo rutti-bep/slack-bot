9a12b4851b733168bcd4e79a993a4f04599174d5
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-01-24 12:36:06 +0900

    send message

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 var request = require('request');
 var settings = require('./setting.json');
 
+console.log(settings);
+
+request.post('https://slack.com/api/chat.postMessage',
+    {form: {
+            token: settings.token,
+            channel: settings.channel,
+            username: settings.username,
+            text: 'てすとー!!!!!!!',
+            as_user: true
+    }}
+    , (error, res, body) => {
+        console.log(error);
+        console.log(res);
+        console.log(body);
+    }
+)
