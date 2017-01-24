234c686774e0b0cc395b1c9e23deeaf24ccaa0d5
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-01-24 19:32:03 +0900

    fix send schedule

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var slack = require('./slack');
 slack.send("起動しますた","phh-bot-training");
 var startLesson = new CronJob("0 30 10-16 * * 1-5",function(){slack.send("授業開始だよ!")},null,true,"Asia/Tokyo")
 var endLesson = new CronJob("0 20 11-16 * * 1-5",function(){slack.send("休み時間だよ!")},null,true,"Asia/Tokyo")
+var endLassonOnDay = new  CronJob("0 20 17 * * 1-4",function(){slack.send("今日はこれで終わりだよ!お疲れー!")},null,true,"Asia/Tokyo")
+var endLassonOnFriDay = new  CronJob("0 20 17 * * 5",function(){slack.send("後はHRで終わりだよ！１週間お疲れー!")},null,true,"Asia/Tokyo")
 //var test = new CronJob("0 * * * * *",function(){slack.send("cronテスト")},null,true,"Asia/Tokyo")
-//var moveIdentify = new CronJob("0 * * * * *",function(){console.log("move!");},null,true,"Asia/Tokyo")
 
 slack.teamGet();
