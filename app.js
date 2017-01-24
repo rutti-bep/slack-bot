1035e2644c643ef17eeca162a2b8e78fe409bd1e
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-01-24 14:17:19 +0900

    moveIdentify cronjob

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var username = process.env.SLACKBOT_USERNAME;
 post("起動しますた");
 var startLesson = new CronJob("0 30 10-16 * * 1-5",function(){post("授業開始だよ!")},null,true,"Asia/Tokyo")
 var endLesson = new CronJob("0 20 11-16 * * 1-5",function(){post("休み時間だよ!")},null,true,"Asia/Tokyo")
+var moveIdentify = new CronJob("0 * * * * *",function(){console.log("move!");},null,true,"Asia/Tokyo")
 
 function post(text){
     request.post('https://slack.com/api/chat.postMessage',
