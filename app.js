98fc7d6238e7358dfa5bc273bde6b8e7616624b3
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-01-24 13:06:19 +0900

    herokuのdeploy準備

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 var request = require('request');
+var CronJob = require('cron').CronJob;
+
+new CronJob('')
+
 var settings = require('./setting.json');
 
-console.log(settings);
+var startLesson = new CronJob("0 30 10-16 * * 1-5",function(){post("授業開始だよ!")},null,true,"Asia/Tokyo")
+var endLesson = new CronJob("0 20 11-16 * * 1-5",function(){post("休み時間だよ!")},null,true,"Asia/Tokyo")
 
-request.post('https://slack.com/api/chat.postMessage',
-    {form: {
+function post(text){
+    request.post('https://slack.com/api/chat.postMessage',
+       {form: {
             token: settings.token,
             channel: settings.channel,
             username: settings.username,
-            text: 'てすとー!!!!!!!',
-            as_user: true
-    }}
-    , (error, res, body) => {
-        console.log(error);
-        console.log(res);
-        console.log(body);
-    }
-)
+            as_user: true,
+            text: text
+        }}
+        , (error, res, body) => {
+            console.log(error);
+            console.log(res);
+        }
+    )
+}
