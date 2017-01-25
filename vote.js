2a7babb8a8ac6bdf4dee4ece69f824db3a1724c6
himanoa <matsunoappy@gmail.com>
2017-01-25 12:04:00 +0900

    WIP: implement filterd emojis

--- vote.js
+++ vote.js
@@ -1,7 +1,11 @@
 'use strict'
+const request = require('request');
 function vote(text, teamName, toolKit) {
-  console.log("text:", text);
-  toolKit.send(text, teamName);
+  const vote_text = text.slice(text.indexOf("vote"));
+  const emojis = vote_text.match(/:([^\s]+):/g);
+  if(emojis == null){
+    return ;
+  }
 }
 
 module.exports = {vote: vote};
