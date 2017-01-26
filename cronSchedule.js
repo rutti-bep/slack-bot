1b8c1574651bce25241b51d8b101dd76ee62832c
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-01-26 12:29:05 +0900

    remove testCode

--- cronSchedule.js
+++ cronSchedule.js
@@ -6,7 +6,6 @@ function scheduleStart(slackSend){
     var endLesson = new CronJob("0 20 11-16 * * 1-5",function(){slackSend("休み時間だよ!")},null,true,"Asia/Tokyo")
     var endLassonOnDay = new  CronJob("0 20 17 * * 1-4",function(){slackSend("今日はこれで終わりだよ!お疲れー!")},null,true,"Asia/Tokyo")
     var endLassonOnFriDay = new  CronJob("0 20 17 * * 5",function(){slackSend("後はHRで終わりだよ！１週間お疲れー!")},null,true,"Asia/Tokyo")
- var test = new CronJob("0 * * * * *",function(){slackSend("cronテスト")},null,true,"Asia/Tokyo")
     
 }
 
