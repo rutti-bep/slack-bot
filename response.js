094e2652286afd98db03ad7c642df2f67e7047ff
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-02-02 15:37:40 +0900

    add response 卵焼き食べりゅ？

--- response.js
+++ response.js
@@ -8,6 +8,9 @@ var responseFunction = function(res,teamName,toolKit){
     if(res.text.match(/(\S死|バグ|困)/g)){
       toolKit.send("プ ロ セ ス よ\n死 ん で し ま う と は\nな さ け な い",teamName);
     }
+    if(res.text.match(/(腹|はら)(減った|へった)/g)){
+      toolKit.send("卵焼き食べりゅ？",teamName);
+    }
     if(res.text && res.text.match(/vote/g)){
       vote.vote(res, teamName, toolKit);
     }
