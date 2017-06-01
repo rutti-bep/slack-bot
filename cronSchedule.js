b7accc5409ce2eac7cd0e7a8dc8f2542ab412b55
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-06-01 15:22:39 +0900

    add syaroChallenge

--- cronSchedule.js
+++ cronSchedule.js
@@ -1,10 +1,12 @@
 "use strict";
 var CronJob = require('cron').CronJob;
+const syaroChallenge = require("./syaroChallenge");
 
 function scheduleStart(slackSend){
   var startLessonOnAm = new CronJob("0 30 10 1 4 *",function(){slackSend("<!channel> 授業開始だよ!")},null,true,"Asia/Tokyo")
   var aprilComment = new  CronJob("30 30 10 1 4 *",function(){slackSend("<!channel> なんてな！エイプリルフールだよ！！")},null,true,"Asia/Tokyo")
-    
+  
+  var syaroChan = new CronJob('0 0 * * * *',function(){slackSend(syaroChallenge)},null,true,"Asia/Tokyo");
 }
 
 module.exports = {scheduleStart: scheduleStart};
