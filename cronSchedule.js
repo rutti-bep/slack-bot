4d461e975be7dd5561c2a1bf4f34ace860c255ca
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-06-01 17:47:43 +0900

    fix bug

--- cronSchedule.js
+++ cronSchedule.js
@@ -6,7 +6,7 @@ function scheduleStart(slackSend){
   var startLessonOnAm = new CronJob("0 30 10 1 4 *",function(){slackSend("<!channel> 授業開始だよ!")},null,true,"Asia/Tokyo")
   var aprilComment = new  CronJob("30 30 10 1 4 *",function(){slackSend("<!channel> なんてな！エイプリルフールだよ！！")},null,true,"Asia/Tokyo")
   
-  var syaroChan = new CronJob('0 0 * * * *',function(){slackSend(syaroChallenge)},null,true,"Asia/Tokyo");
+  var syaroChan = new CronJob('0 0 * * * *',function(){slackSend(syaroChallenge())},null,true,"Asia/Tokyo");
 }
 
 module.exports = {scheduleStart: scheduleStart};
